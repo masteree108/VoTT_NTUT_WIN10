eb544bf7f3306bb0f8ee75969a1b163ea3f6a8f7
Ivan yu <gun123kk@gmail.com>
2020-10-22 01:20:54 +0800

    (NTUT)(SERVICE) using setInterval() to relpace while(1) in the service
    if you want to use this function, please modify below
      variable before you execute the VoTT
    $ vim VoTT_NTUT/WIN10/src/electron/start.js
    -const detect_tag_switch = false;
    +const detect_tag_switch = true;
    
    the details please refer below doc:
    https://hackmd.io/@NTUTVOTT/HyrJJtDvP
    
    Dependence:
      VoTT_NTUT/WIN10

--- NTUT/services/run_exe.js
+++ NTUT/services/run_exe.js
@@ -1,12 +1,10 @@
 const { parentPort } = require('worker_threads');
-const fs = require('fs');    
+const fs = require('fs');
 var now_time = "1";     
 var before_time= "0";   
 var execFile = require('child_process').execFile;
 var parameters = ["--incognito"];
          
-//can not use below function otherwise that will cause javascript out of memory
-/*       
 function open_path(path) {                                                                                                                                        
 	fs.open(path, 'r', (err, fd) => {
        if (err) throw err;   
@@ -21,8 +19,8 @@ function open_path(path) {
          
 function get_time(data) {    
    var res = data.split(",");
-   console.log("res[2]");    
-   console.log(res[2]);      
+   //console.log("res[2]");    
+   //console.log(res[2]);      
    return res[2];
 }        
          
@@ -37,30 +35,19 @@ function already_run_check() {
 
 function read_file(path) {   
    get_data = fs.readFileSync(path, 'utf8');
-   console.log(get_data);    
+   //console.log(get_data);    
    now_time = get_time(get_data);
-   console.log(now_time);    
+   //console.log(now_time);    
    return already_run_check();
                              
 }                            
-
-// sleep time expects milliseconds
-function sleep (time) {      
-	return new Promise((resolve) => setTimeout(resolve, time));
-}                            
-*/ 
-                             
-while(1) {                   
+                           
+function run_vott_tracker_exe_checek() {
 	log_path = '../../Drone_Project/Drone_Target/for_python_path.log';
 	exe_path = './NTUT/exe/vott_tracker.exe'
 	if (fs.existsSync(log_path)) {  
 		try {                 
-			get_data = fs.readFileSync(log_path, 'utf8');
-			res = get_data.split(",");
-			now_time = res[2];
-			//using file created time to judge this is new track object
-			if (now_time != before_time) {
-				//console.log(now_time);
+			if (read_file(log_path)) {
 				if (fs.existsSync(exe_path)) {
 					before_time = now_time;
 					execFile('./NTUT/exe/vott_tracker.exe', parameters, function(err, data) {
@@ -81,6 +68,9 @@ while(1) {
 		}
 	}                                 
 }
+
+setInterval(() => {
+  run_vott_tracker_exe_checek()}, 100);
 
 /* parentPort.postMessage() is the send message function, 
 	it will send to below position(resovle) that thread creator
