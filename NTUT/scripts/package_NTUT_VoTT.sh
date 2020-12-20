61ad5a1dbd9440c95d247defe5b3148110e03795
ivan <gun123kk@gmail.com>
2020-12-20 23:34:20 +0800

    (NTUT)(SCRIPT) modify package_NTUT_VoTT.sh to remove some redundant command
    if you are executing build exe command
    $ npm run release
    it will shows the below log, as you can see it already included
    run_vott_tracker.js file
    [153] ./NTUT/services/run_vott_tracker.js 1.88 KiB {0} [built]
    
    so modify script to remove copy run_vott_tracker command

--- NTUT/scripts/package_NTUT_VoTT.sh
+++ NTUT/scripts/package_NTUT_VoTT.sh
@@ -7,9 +7,7 @@ if [ -d "../../releases" ]; then
 	mv win-unpacked WIN10
 	cd WIN10
 	mkdir -p NTUT/exe
-	mkdir -p NTUT/services
 	cp ../../NTUT/exe/vott_tracker.exe ./NTUT/exe/
-	cp ../../NTUT/services/run_vott_tracker.js ./NTUT/services/
 	cd ../../
 	mv NTUT_VoTT ../../../
 else
